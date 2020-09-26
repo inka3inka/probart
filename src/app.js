7e214f8a0c81e144302a5c7dcb7aa11b61e2e84d
inka3inka <inka3inka@gmail.com>
2020-09-26 19:51:32 +0200

    Basic styles for realizations section

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import "../styles/main.scss";
 
 const advertise = document.querySelector('.header__advertise');
-const advertiseUrl = '../assets/wooden_floor.jpg'
+const advertiseUrl = '../assets/wooden_floor.jpg';
+const realizationsTile1 = document.querySelector('.realizations__container div:first-of-type');
+const realizationsTile1Url = '../assets/wooden1.jpeg';
+const realizationsTile2 = document.querySelector('.realizations__container div:last-of-type');
+const realizationsTile2Url = '../assets/wooden2.jpeg';
 
 
 // Set background-image for header__advertise - doest't work in css because of webpack configuration
 advertise.setAttribute("style", "background-image: url('"+advertiseUrl+"')")
+realizationsTile1.setAttribute("style", "background-image: url('"+realizationsTile1Url+"')")
+realizationsTile2.setAttribute("style", "background-image: url('"+realizationsTile2Url+"')")
